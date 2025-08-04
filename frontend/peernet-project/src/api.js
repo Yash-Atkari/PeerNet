2174a8a4347400a300028425e84abb8f3ea63632
Yash Atkari <yashatkari7@gmail.com>
2025-08-04 08:11:15 +0530

    Change routes

--- frontend/peernet-project/src/api.js
+++ frontend/peernet-project/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE,
+  BASE_URL: import.meta.env.VITE_API_BASE,
   withCredentials: true,            // ← Allow sending cookies
 });
